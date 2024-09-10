3f0bcb91b3f741153bf30c4c7c245ed089c19de5
Alex Bratov <104260167+THEMETALLEGEND@users.noreply.github.com>
2024-09-10 15:44:37 +0500

    Player with movement and pixel jitter bug
    1. added player object with a sprite model (12 pixels per unit) and basic movement that utilises the new input system
    BUG: the characters' pixel jitter when moving.

---
+++ Assets/Scripts/Test/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody2D rb;
+	public float moveSpeed = 5f;
+	public PlayerActionsInput actionInput;
+	public InputAction move;
+
+	Vector2 moveDirection = Vector2.zero;
+
+	private void Awake()
+	{
+		actionInput = new PlayerActionsInput();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnEnable()
+	{
+		move = actionInput.Player.Move;
+		move.Enable();
+	}
+
+	private void OnDisable()
+	{
+		move.Disable();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		moveDirection = move.ReadValue<Vector2>();
+	}
+
+	private void FixedUpdate()
+	{
+		rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+	}
+}

a6fbd0b50af1ee136a850c7fe6964a15702b3701
Alex Bratov <104260167+THEMETALLEGEND@users.noreply.github.com>
2024-09-16 16:39:46 +0500

    4 directional Idle
    1. Player has an Idle Blend Tree with last known directions. That allows the player to remember its last moving direction, to then transition to Idle animation of the same direciton.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
 
 	Vector2 moveDirection = Vector2.zero;
 
+	// Variables to store the last movement direction
+	private float lastDirX = 0f;
+	private float lastDirY = 0f;
+
 	private void Awake()
 	{
 		actionInput = new PlayerActionsInput();
@@ -38,6 +42,17 @@ public class PlayerMovement : MonoBehaviour
 
 		animator.SetFloat("Horizontal", moveDirection.x);
 		animator.SetFloat("Vertical", moveDirection.y);
+
+		// Save the last movement direction if the player is moving
+		if (moveDirection.magnitude > 0.01f) // If there is movement
+		{
+			lastDirX = moveDirection.x;
+			lastDirY = moveDirection.y;
+		}
+
+		// Send the last direction to the Animator 
+		animator.SetFloat("lastDirX", lastDirX);
+		animator.SetFloat("lastDirY", lastDirY);
 	}
 
 	private void FixedUpdate()

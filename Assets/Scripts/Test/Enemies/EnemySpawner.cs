38007c6cd31751be0a15f95bebb5949b6f5f1e87
Alex Bratov <104260167+THEMETALLEGEND@users.noreply.github.com>
2024-09-21 15:07:14 +0500

    UI scales!
    1. Created new UI template using world canvas. This way it renders through GB shader good and scales perfectly. (THIS METHOD MESSES UP PIXELS FOR SOME REASON - GOTTA LOOK INTO IT)
    2. Edited Spawner code a little bit so it disables sprite renderer on awake.
    3. Added enemy spawner prefabs.

--- Assets/Scripts/Test/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Test/Enemies/EnemySpawner.cs
@@ -4,30 +4,37 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject EnemyToGenerate;
-    public int roomNumber_;
-    bool alreadyGenerated = false;
-    public void SpawnEnemies()
-    {
-        Debug.Log("room number "+roomNumber_);
-        if (EnemyToGenerate != null)
-        {
-            StartCoroutine(GenerateRoutine());
-        }
-    }
-    private IEnumerator GenerateRoutine()
-    {
-        if (!alreadyGenerated)
-        {
-            alreadyGenerated = true;
-            if (EnemyToGenerate != null)
-            {
-                GameObject enemy = Instantiate(EnemyToGenerate, transform.position, Quaternion.identity);
-                enemy.gameObject.SetActive(true);
-                enemy.GetComponent<Enemy>().SetRoom(roomNumber_);
-            }
-            yield return new WaitForSeconds(1f);
-            Destroy(gameObject);
-        }
-    }
+	public GameObject EnemyToGenerate;
+	public int roomNumber_;
+	bool alreadyGenerated = false;
+	public void SpawnEnemies()
+	{
+		Debug.Log("room number " + roomNumber_);
+		if (EnemyToGenerate != null)
+		{
+			StartCoroutine(GenerateRoutine());
+		}
+	}
+
+	private void Awake()
+	{
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		spriteRenderer.enabled = false;
+	}
+
+	private IEnumerator GenerateRoutine()
+	{
+		if (!alreadyGenerated)
+		{
+			alreadyGenerated = true;
+			if (EnemyToGenerate != null)
+			{
+				GameObject enemy = Instantiate(EnemyToGenerate, transform.position, Quaternion.identity);
+				enemy.gameObject.SetActive(true);
+				enemy.GetComponent<Enemy>().SetRoom(roomNumber_);
+			}
+			yield return new WaitForSeconds(1f);
+			Destroy(gameObject);
+		}
+	}
 }

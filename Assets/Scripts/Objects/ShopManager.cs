0ad0e4dd67d08bbc8dd1ac05ec435ac4f2331643
ZoraGabs <52766266+ZoraGabs@users.noreply.github.com>
2024-09-23 01:43:10 -0300

    fixed sound

--- Assets/Scripts/Objects/ShopManager.cs
+++ Assets/Scripts/Objects/ShopManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     public List<ShopItens> items = new List<ShopItens>();
+    public int price_SmallHeart;
+    public int price_Heart;
+    public int price_RNG_Upgrade;
+    public int price_ROF_Upgrade;
 
     void Start()
     {
-        // Apenas um exemplo de como acessar os itens
+        items.Add(new ShopItens(0, "Small Heart", "", price_SmallHeart));
+        items.Add(new ShopItens(1, "Heart", "", price_Heart));
+        items.Add(new ShopItens(2, "RangeUpgrade", "", price_RNG_Upgrade));
+        items.Add(new ShopItens(3, "FireRateUpgrade", "", price_ROF_Upgrade));
+
+
         foreach (var item in items)
         {
             Debug.Log($"Item: {item.itemName}, Description: {item.description}, Price: {item.price}");

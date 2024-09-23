e5db75458a8e4ad4473ea72ad2034bd418391f28
ZoraGabs <52766266+ZoraGabs@users.noreply.github.com>
2024-09-23 03:37:48 -0300

    stalker time to attack fixed

--- Assets/Scripts/Test/Enemies/Stalker_Script.cs
+++ Assets/Scripts/Test/Enemies/Stalker_Script.cs
@@ -67,7 +67,7 @@ public class Stalker_Script : MonoBehaviour, Enemy
 	{
 		AIPath ai = GetComponent<AIPath>();
 		ai.canMove = false;
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(0.1f);
 		ai.canMove = true;
 	}
 
